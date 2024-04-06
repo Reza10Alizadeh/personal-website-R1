831639f0e2db3257ab278fb130f06a753c9e1e5c
Reza10Alizadeh <157479001+Reza10Alizadeh@users.noreply.github.com>
2024-04-06 16:05:31 +0330

    build about me section.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab-links");
+            var tabcontents = document.getElementsByClassName("tab-contents");
+            function opentab(tabname) {
+              for (tablink of tablinks) {
+                tablink.classList.remove("active");
+              }
+              for (tabcontent of tabcontents) {
+                tabcontent.classList.remove("active-skills"); // Fixed variable name here
+              }
+              event.currentTarget.classList.add("active");
+              document.getElementById(tabname).classList.add('active-skills')
+            }
